74e6fa62c4e660f3213106900a685c49729227b7
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-03 21:53:11 +0200

    Prepare this repository from submodule

---
+++ flex-gcc.m
@@ -0,0 +1,120 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% Copyright (C) 2022 Kevin Matthes
+%%
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 2 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License along
+%% with this program; if not, write to the Free Software Foundation, Inc.,
+%% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%%
+%%%%
+%%
+%%  FILE
+%%      flex-gcc.m
+%%
+%%  BRIEF
+%%      Create a new lexer using Flex and GCC.
+%%
+%%  AUTHOR
+%%      Kevin Matthes
+%%
+%%  COPYRIGHT
+%%      (C) 2022 Kevin Matthes.
+%%      This file is licensed GPL 2 as of June 1991.
+%%
+%%  DATE
+%%      2022
+%%
+%%  NOTE
+%%      See `LICENSE' for full license.
+%%      See `README.md' for project details.
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%
+%%
+%% Variables.
+%%
+%%%%
+
+% Software.
+compiler.flags  = ' -Wall -Werror -Wextra -Wno-unused-function -Wpedantic ';
+compiler.in     = '*.c';
+compiler.link   = ' -lfl ';
+compiler.out    = 'untab';
+compiler.self   = ' gcc ';
+compiler.call   = [compiler.self ' ' compiler.flags ' ' compiler.in];
+compiler.call   = [compiler.call ' ' compiler.link ' -o ' compiler.out];
+
+scangen.in      = 'untab.l';
+scangen.out     = 'lex.yy.c';
+scangen.self    = ' flex ';
+scangen.call    = [scangen.self ' ' scangen.in];
+
+
+
+% Files.
+files.self  = 'flex-gcc.m';
+
+
+
+% Miscellaneous.
+banner  = ['[ ' files.self ' ] '];
+
+
+
+%%%%
+%%
+%% Build steps.
+%%
+%%%%
+
+% Begin build instruction.
+disp ([banner 'Begin build instruction.']);
+
+
+
+% Call Flex.
+disp ([banner 'Compile scanner definition with Flex ...']);
+
+disp (scangen.call);
+system (scangen.call);
+
+disp ([banner 'Done.']);
+
+
+
+% Call GCC.
+disp ([banner 'Compile C source code with GCC ...']);
+
+disp (compiler.call);
+system (compiler.call);
+
+disp ([banner 'Done.']);
+
+
+
+% Clean build artifacts.
+fprintf ([banner 'Remove build artifacts ... ']);
+
+if length (glob (scangen.out));
+    delete (scangen.out);
+end;
+
+disp ('Done.');
+
+
+
+% End build instruction.
+disp ([banner 'End build instruction.']);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ install-app.m
@@ -0,0 +1,114 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+%% Copyright (C) 2022 Kevin Matthes
+%%
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 2 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License along
+%% with this program; if not, write to the Free Software Foundation, Inc.,
+%% 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%%
+%%%%
+%%
+%%  FILE
+%%      install-app.m
+%%
+%%  BRIEF
+%%      Install a compilation of the provided application.
+%%
+%%  AUTHOR
+%%      Kevin Matthes
+%%
+%%  COPYRIGHT
+%%      (C) 2022 Kevin Matthes.
+%%      This file is licensed GPL 2 as of June 1991.
+%%
+%%  DATE
+%%      2022
+%%
+%%  NOTE
+%%      See `LICENSE' for full license.
+%%      See `README.md' for project details.
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%
+%%
+%% Variables.
+%%
+%%%%
+
+% Software.
+octave.in   = 'flex-gcc.m';
+octave.out  = './untab';
+octave.self = ' octave ';
+octave.call = [octave.self ' ' octave.in];
+
+% Directories.
+directories.dep1    = '~/.local/';
+directories.target  = '~/.local/bin/';
+
+% Files.
+files.self  = 'install-app.m';
+
+% Miscellaneous.
+banner  = ['[ ' files.self ' ] '];
+
+
+
+%%%%
+%%
+%% Build steps.
+%%
+%%%%
+
+% Begin build instruction.
+disp ([banner 'Begin build instruction.']);
+
+
+
+% Compile application.
+disp ([banner 'Compile the application to install ...']);
+system (octave.call);
+disp ([banner 'Done.']);
+
+
+
+% Create directories, if required.
+fprintf ([banner 'Check for ' directories.dep1 ' ... ']);
+if length (glob (directories.dep1));
+    disp ('Done.');
+else;
+    mkdir (directories.dep1);
+    disp ('Created.');
+end;
+
+fprintf ([banner 'Check for ' directories.target ' ... ']);
+if length (glob (directories.target));
+    disp ('Done.');
+else;
+    mkdir (directories.target);
+    disp ('Created.');
+end;
+
+
+
+% Install.
+fprintf ([banner 'Install ' octave.out ' in ' directories.target ' ... ']);
+movefile (octave.out, directories.target, 'f');
+disp ('Done.');
+
+
+
+% End build instruction.
+disp ([banner 'End build instruction.']);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
